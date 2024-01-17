b8871400f6e4e28517f148653e429a19f098dbf1
yagnik <yagnik.diyora@upayments.com>
2024-01-17 14:02:28 +0530

    final commit (yagnik)

--- src/components/App.js
+++ src/components/App.js
@@ -74,6 +74,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
